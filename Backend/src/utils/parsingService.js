315b6e28cf2d8a5f7db90341fff9a2d31aad9461
Suman Kumar Sahu <sumankumarsahu221@gmail.com>
2025-12-31 08:12:02 +0530

    Refactor PDF parsing logic to enhance text processing and formatting

--- Backend/src/utils/parsingService.js
+++ Backend/src/utils/parsingService.js
@@ -1,17 +1,23 @@
 import pdf from "pdf-parse-fixed";
 
 export const parseToText = async (buffer) => {
-    try {
-        const data = await pdf(buffer);
-        let text = data.text
-            .replace(/\n/g, " ")
-            .replace(/[^a-zA-Z0-9., ]/g, " ")
-            .replace(/\s+/g, " ")
-            .toLowerCase()
-            .trim();
-        return text;  
-    } catch (error) {
-        console.error("PDF parse error:", error);
-        throw new Error("PDF parsing failed");
-    }
+  try {
+    const data = await pdf(buffer);
+    let text = data.text;
+
+    text = text.replace(/\s+/g, " ");
+
+    text = text.replace(/\b(C\+\+|C#|F#)\b/gi, (m) => m.toUpperCase());
+
+    text = text.replace(/\b([A-Za-z]+)\.(js|ts|jsx|tsx)\b/gi,(_, name, ext) => `${name.toLowerCase()} ${ext.toLowerCase()}`);
+
+    text = text.replace(/(?<=\w)\.(?=\w)/g, " ");
+    
+    text = text.toLowerCase();
+
+    return text;
+  } catch (error) {
+    console.error("PDF parse error:", error);
+    throw new Error("PDF parsing failed");
+  }
 };
