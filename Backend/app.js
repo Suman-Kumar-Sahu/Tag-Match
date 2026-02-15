6fb088f77bbb3bde3aab28444a70e0c19624099e
Suman Kumar Sahu <147469858+Suman-Kumar-Sahu@users.noreply.github.com>
2026-02-15 12:21:19 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ import atsRoutes from './src/routes/atsRoutes.js';
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173,
+    origin:"http://localhost:5173",
     //"http://localhost:5173" 
     credentials:true
 }))
