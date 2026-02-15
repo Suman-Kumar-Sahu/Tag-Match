ed35aef7c3fc59f6316f9cfd102880385abb4ddc
Suman Kumar Sahu <147469858+Suman-Kumar-Sahu@users.noreply.github.com>
2026-02-15 12:38:28 +0530

    Update API base URL to include '/api/ats'

--- Frontend/client/src/utils/api.js
+++ Frontend/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://tag-match.onrender.com", 
+  baseURL: "https://tag-match.onrender.com/api/ats", 
 });
 
 export const uploadResume = (data) => {
