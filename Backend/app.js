cae16b001b32e7775693deb96f631b9527cc9563
Suman Kumar Sahu <147469858+Suman-Kumar-Sahu@users.noreply.github.com>
2026-02-15 12:22:18 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ import atsRoutes from './src/routes/atsRoutes.js';
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://tag-match-frontend.onrender.com",
     //"http://localhost:5173" 
     credentials:true
 }))
