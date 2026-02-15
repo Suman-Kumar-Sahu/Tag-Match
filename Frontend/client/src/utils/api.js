c8992dd66910df34375ce37e0ac61274f556ea8a
Suman Kumar Sahu <147469858+Suman-Kumar-Sahu@users.noreply.github.com>
2026-02-15 12:55:24 +0530

    Update api.js

--- Frontend/client/src/utils/api.js
+++ Frontend/client/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://tag-match.onrender.com/api/ats", 
+  baseURL: "https://tag-match.onrender.com/api/ats/", 
 });
 
 export const uploadResume = (data) => {
-  return API.post("/analyze", data);
+  return API.post("analyze", data);
 };
